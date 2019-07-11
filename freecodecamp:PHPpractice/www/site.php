964ef08cfd286c299864bdc31733958acd64f763
tcuddle <thcrump38@yahoo.com>
2019-07-11 17:58:03 -0500

    ASSOCIATIVE ARRAYS, posting ass.arrays through form submission

--- freecodecamp:PHPpractice/www/site.php
+++ freecodecamp:PHPpractice/www/site.php
@@ -13,16 +13,15 @@
 
 
 <form action="site.php" method="post">
-    Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
-    Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
-    Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+    <input type="text" name="student">
     <input type="submit">
 </form>
 
 
 <?php
-    $fruits = $_POST["fruits"];
-    echo $fruits[0];
+    $grades = array("Jim"=>"A+", "Pam"=>"B-", "Oscar"=>"C+");
+
+    echo $grades[$_POST["student"]];
 
 ?>
 
