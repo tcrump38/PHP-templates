d05e8755203361cd83413707a426d71a91bd1b7f
tcuddle <thcrump38@yahoo.com>
2019-07-13 15:36:39 -0500

    SWITCH stmnts

--- freecodecamp:PHPpractice/www/site.php
+++ freecodecamp:PHPpractice/www/site.php
@@ -12,28 +12,23 @@
     
 
 <form action="site.php" method="post">
-    First Num:<input type="number" step="0.001" name="num1"> <br>
-    OP:<input type="text" name="op"> <br>
-    Second Num:<input type="number" step="0.001" name="num2"> <br>
+    <input type="text" name="grade">
     <input type="submit">
 </form>
 
 
-<?php
-$num1 = $_POST["num1"];
-$num2 = $_POST["num2"];
-$op = $_POST["op"];
 
-if($op == "+"){
-    echo $num1 + $num2;
-}elseif($op == "-"){
-    echo $num1 - $num2;
-}elseif($op == "/"){
-    echo $num1 / $num2;
-}elseif($op == "*"){
-    echo $num1 * $num2;
-}else{
-    echo "invalid operator";
+<?php
+$grade = $_POST["grade"];
+switch ($grade){
+    case "A":
+        echo "You did amazing";
+        break;
+    case "B":
+        echo "You did pretty good";
+        break;
+    default:
+        echo "Invalid grade";
 }
 
 
